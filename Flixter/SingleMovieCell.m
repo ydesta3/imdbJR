2611535c2e341196ba54d037fa5120a99ed42cf5
Yonatan Desta <ydesta@fb.com>
2022-06-20 02:54:16 -0700

    trial error

---
+++ Flixter/SingleMovieCell.m
@@ -0,0 +1,21 @@
+//
+//  singleMovieCell.m
+//  Flixter
+//
+//  Created by Yonatan Desta on 6/17/22.
+//
+
+#import "singleMovieCell.h"
+
+@implementation singleMovieCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
---
+++ Flixter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flixter
+//
+//  Created by Yonatan Desta on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
